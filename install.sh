fd89c25c0ef2f1e5e8140ce590120861463ce487
dejan995 <40325303+dejan995@users.noreply.github.com>
2021-06-20 11:40:41 +0200

    Added a simle install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir /mnt/docker #Creates the main docker folder for storing configurations
+mkdir -p /mnt/docker/smbwebmin/webmin
+mkdir -p /mnt/docker/smbwebmin/samba/etc
+mkdir -p /mnt/docker/smbwebmin/samba/var/lib
