1144d87bc0912ba42478cf42d6ab1dcf95183d5f
dejan995 <40325303+dejan995@users.noreply.github.com>
2021-06-23 21:32:53 +0200

    Update

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 echo '####################################################'
 echo 'Setting up Docker Repository'
 echo '####################################################'
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y &>>/dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg &>>/dev/null
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
